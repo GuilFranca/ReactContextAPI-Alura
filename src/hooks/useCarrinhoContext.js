e77cfc427c39e7a26a10ea2a6771e49618acfe52
Guilherme França <guilfranca27@gmail.com>
2026-08-31 23:26:11 -0300

    Aula 5 - 3. Utilizando o useReducer Finalizada

--- src/hooks/useCarrinhoContext.js
+++ src/hooks/useCarrinhoContext.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useMemo } from "react"
 import { CarrinhoContext } from "../context/CarrinhoContext"
 
 export const useCarrinhoContext = () => {
@@ -56,8 +56,9 @@ export const useCarrinhoContext = () => {
         )
     }
 
-    useEffect(() => {
-        const { totalTemp, quantidadeTemp } = carrinho.reduce((acumulador, produto) => ({
+
+    const { totalTemp, quantidadeTemp } = useMemo(() => {
+        return carrinho.reduce((acumulador, produto) => ({
             quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
             totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
         }),
@@ -66,9 +67,13 @@ export const useCarrinhoContext = () => {
                 totalTemp: 0
             }
         );
+    }, [carrinho])
+
+
+    useEffect(() => {
         setQuantidade(quantidadeTemp);
         setValorTotal(totalTemp)
-    }, [carrinho])
+    })
 
     return {
         carrinho,
